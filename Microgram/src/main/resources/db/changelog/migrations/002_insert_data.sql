fa19043ce6ed9edaa4d2e258d99f7f454f92dd06
Askar <nurbekovaskar099@gmail.com>
2026-05-20 23:22:34 +0600

    created tables

---
+++ Microgram/src/main/resources/db/changelog/migrations/002_insert_data.sql
@@ -0,0 +1,8 @@
+﻿INSERT INTO usr (username, email, password, full_name, bio)
+VALUES ('admin', 'admin@mail.com', '$2a$12$WB2YUbFcCN0tm44SBcKUjua9yiFBsfB3vW02IjuwzY7HGtlQIKzy2', 'Admin',
+        'Site administrator'),
+       ('john', 'john@mail.com', '$2a$12$WB2YUbFcCN0tm44SBcKUjua9yiFBsfB3vW02IjuwzY7HGtlQIKzy2', 'John Doe',
+        'Just a user'),
+       ('jane', 'jane@mail.com', '$2a$12$WB2YUbFcCN0tm44SBcKUjua9yiFBsfB3vW02IjuwzY7HGtlQIKzy2', 'Jane Smith',
+        'Another user');
+-- пароль для всех: qwe
